8682e984c31634cd9f89cee12a5b1885c808782e
SejalCh <139526001+SejalCh@users.noreply.github.com>
2023-10-23 22:12:06 +0530

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *var1, int *var2)
+{
+    int temp = *var1;
+    *var1 = *var2;
+    *var2 = temp;
+}
+//function for Bubble Sort.
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)
+       
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+               swap(&arr[j], &arr[j+1]);
+}
+// Function to print array.
+void display(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout << arr[i] << "\t";
+        
+    cout<<endl;
+}
+
+int main()
+{
+    int array[] = {-11,20,4,15,-1};//we can take input from the user also
+    int size = sizeof(array)/sizeof(array[0]);
+    
+    cout<<"Before bubble sort: \n";
+    display(array, size);//to print unsorted array.
+    
+    bubbleSort(array, size);
+    
+    cout<<"After bubble sort: \n";
+    display(array, size);//to print sorted array.
+    
+    return 0;
+}
+/*output
+Before bubble sort: 
+-11     20      4       15      -1
+After bubble sort: 
+-11     -1      4       15      20*/
